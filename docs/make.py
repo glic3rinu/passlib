73c49e8b127ff0ec9fc80a7c3a21d610e159578c
Eli Collins <elic@assurancetechnologies.com>
2011-01-07 21:28:41 +0000

    doc building now uses astdoc, updated masthead & logo

--- docs/make.py
+++ docs/make.py
@@ -1,8 +1,5 @@
 "Makefile for Sphinx documentation, adapted to python"
-import os, sys
-doc_root = os.path.abspath(os.path.join(__file__,os.path.pardir))
-source_root = os.path.abspath(os.path.join(doc_root,os.path.pardir))
-sys.path.insert(0, source_root)
-
-from bps.unstable.bpsdoc.make_helper import SphinxMaker
-SphinxMaker.execute(root=__file__)
+import os
+from astdoc.make_helper import SphinxMaker
+if __name__ == "__main__":
+    SphinxMaker.execute(root_dir=os.path.join(__file__,os.pardir))
