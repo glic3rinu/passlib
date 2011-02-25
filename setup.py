b1d23b79c8c621b6327f7dfe38b6b6b7f6834393
Eli Collins <elic@assurancetechnologies.com>
2011-02-25 15:45:21 -0500

    updated info in setup.py

--- setup.py
+++ setup.py
@@ -15,17 +15,41 @@ from passlib import __version__ as version
 setup(
     #package info
     packages = find_packages(),
+    package_data = { "passlib": ["*.cfg"] },
 
     # metadata
     name = "passlib",
     version = version,
     author = "Eli Collins",
-    author_email = "elic@astllc.org",
-    description = "utilities for password generation & hashing",
+    author_email = "elic@assurancetechnologies.com",
     license = "BSD",
-    keywords = "password hash generation secret security sha md5 bcrypt crypt",
-    url = "http://www.astllc.org/software/passlib",
-    # could also include long_description, download_url, classifiers, etc.
+
+    url = "http://www.assurancetechnologies.com/software/passlib",
+    #download_url
+
+    description = "password hash library",
+    long_description = """\
+Passlib provides cross-platform implementations of most of the major
+password hashing algorithms; as well as a framework for managing
+and migrating existing password hashes. It's designed to be useful
+for anything from quickly verify a hash found in /etc/shadow,
+to integrating full-strength password hashing for multi-user application.
+""",
+
+    keywords = "password secret hash security crypt md5-crypt sha256-crypt sha512-crypt bcrypt",
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Environment :: Plugins",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.5",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Security :: Cryptography",
+        "Topic :: Software Development :: Libraries",
+    ],
     zip_safe=True,
 )
 #=========================================================
