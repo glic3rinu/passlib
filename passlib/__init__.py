7d88ab446005532a5c7c0a0e51c42d772c495954
Eli Collins <elic@assurancetechnologies.com>
2011-04-05 18:25:58 -0400

    tweaked setup.cfg

--- passlib/__init__.py
+++ passlib/__init__.py
@@ -1,13 +1,3 @@
 """passlib - suite of password hashing & generation routinges"""
 
-#====================
-#master version field
-#====================
-#__version__ = "1.4"
-import time
-__version__ = "1.4.dev" + time.strftime("%Y%m%d")
-del time
-
-#===================='
-#eof
-#====================
+__version__ = "1.4"
