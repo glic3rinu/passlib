0f29de787af6ea9876321c0841591087ba71a12f
Eli Collins <elic@assurancetechnologies.com>
2011-10-09 15:07:14 -0400

    typos in setup description

--- setup.py
+++ setup.py
@@ -102,8 +102,8 @@ Passlib is a password hashing library for Python 2 & 3,
 which provides cross-platform implementations of over 20
 password hashing algorithms, as well as a framework for
 managing existing password hashes. It's designed to be useful
-for a wide range of tasks, from verifying a hash found in /etc/shadow,
-to providing full-strength password hashing for multi-user application.
+for a wide range of tasks; from verifying a hash found in /etc/shadow,
+to providing full-strength password hashing for multi-user applications.
 
 * See the `online documentation <http://packages.python.org/passlib>`_
   for details, installation instructions, and examples.
@@ -112,7 +112,7 @@ to providing full-strength password hashing for multi-user application.
   for the latest news, more information, and additional downloads.
 
 * See the `changelog <http://packages.python.org/passlib/history.html>`_
-  for description of what's new in Passlib.
+  for a description of what's new in Passlib.
 
 All releases are signed with the gpg key
 `4CE1ED31 <http://pgp.mit.edu:11371/pks/lookup?op=get&search=0x4D8592DF4CE1ED31>`_.
