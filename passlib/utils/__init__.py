ed508c71b1abce3660f8bdee5b2abd41657eefe0
Eli Collins <elic@assurancetechnologies.com>
2011-12-13 16:21:29 -0500

    issue 24: added saslprep() function to support SASL stringprep profile; still have to work it into Context object

--- passlib/utils/__init__.py
+++ passlib/utils/__init__.py
@@ -13,7 +13,9 @@ from math import log as logb
 import os
 import sys
 import random
+import stringprep
 import time
+import unicodedata
 from warnings import warn
 #site
 #pkg
@@ -40,6 +42,10 @@ __all__ = [
     'to_unicode',
     'to_native_str',
     'is_same_codec',
+
+    # string manipulation
+    'consteq',
+    'saslprep',
 
     #byte manipulation
     "xor_bytes",
@@ -458,6 +464,9 @@ def is_ascii_safe(source):
 #=================================================================================
 #string helpers
 #=================================================================================
+UEMPTY = u""
+USPACE = u" "
+ujoin = UEMPTY.join
 
 def consteq(left, right):
     """check two strings/bytes for equality, taking constant time relative
@@ -534,6 +543,118 @@ def splitcomma(source, sep=","):
         if elem.strip()
     ]
 
+def saslprep(source, errname="value"):
+    """normalizes unicode string using SASLPrep stringprep profile.
+
+    The SASLPrep profile is defined in :rfc:`4013`.
+    It provides a uniform scheme for normalizing unicode usernames
+    and passwords before performing byte-value sensitive operations
+    such as hashing. Among other things, it normalizes diacritic
+    representations, removes non-printing characters, and forbids
+    invalid characters such as ``\n``.
+
+    :arg source:
+        unicode string to normalize & validate
+
+    :param errname:
+        optionally override noun used to refer to source in error messages,
+        defaults to ``value``; mainly useful to make caller's error
+        messages make more sense.
+
+    :raises ValueError:
+        if any characters forbidden by the SASLPrep profile are encountered.
+
+    :returns:
+        normalized unicode string
+    """
+    # saslprep - http://tools.ietf.org/html/rfc4013
+    # stringprep - http://tools.ietf.org/html/rfc3454
+    #              http://docs.python.org/library/stringprep.html
+
+    # validate type
+    if not isinstance(source, unicode):
+        raise TypeError("input must be unicode string, not %s" %
+                        (type(source),))
+
+    # mapping stage
+    #   - map non-ascii spaces to U+0020 (stringprep C.1.2)
+    #   - strip 'commonly mapped to nothing' chars (stringprep B.1)
+    in_table_c12 = stringprep.in_table_c12
+    in_table_b1 = stringprep.in_table_b1
+    data = ujoin(
+        USPACE if in_table_c12(c) else c
+        for c in source
+        if not in_table_b1(c)
+        )
+
+    # normalize to KC form
+    data = unicodedata.normalize('NFKC', data)
+    if not data:
+        return UEMPTY
+
+    # check for invalid bi-directional strings.
+    # stringprep requires the following:
+    #   - chars in C.8 must be prohibited.
+    #   - if any R/AL chars in string:
+    #       - no L chars allowed in string
+    #       - first and last must be R/AL chars
+    # this checks if start/end are R/AL chars. if so, prohibited loop
+    # will forbid all L chars. if not, prohibited loop will forbid all
+    # R/AL chars instead. in both cases, prohibited loop takes care of C.8.
+    is_ral_char = stringprep.in_table_d1
+    if is_ral_char(data[0]):
+        if not is_ral_char(data[-1]):
+            raise ValueError("malformed bidi sequence in " + errname)
+        # forbid L chars within R/AL sequence.
+        is_forbidden_bidi_char = stringprep.in_table_d2
+    else:
+        # forbid R/AL chars if start not setup correctly; L chars allowed.
+        is_forbidden_bidi_char = is_ral_char
+
+    # check for prohibited output - stringprep tables A.1, B.1, C.1.2, C.2 - C.9
+    in_table_a1 = stringprep.in_table_a1
+    in_table_c21_c22 = stringprep.in_table_c21_c22
+    in_table_c3 = stringprep.in_table_c3
+    in_table_c4 = stringprep.in_table_c4
+    in_table_c5 = stringprep.in_table_c5
+    in_table_c6 = stringprep.in_table_c6
+    in_table_c7 = stringprep.in_table_c7
+    in_table_c8 = stringprep.in_table_c8
+    in_table_c9 = stringprep.in_table_c9
+    for c in data:
+        # check for this mapping stage should have removed
+        assert not in_table_b1(c), "failed to strip B.1 in mapping stage"
+        assert not in_table_c12(c), "failed to replace C.1.2 in mapping stage"
+
+        # check for forbidden chars
+        if in_table_a1(c):
+            raise ValueError("unassigned code points forbidden in " + errname)
+        if in_table_c21_c22(c):
+            raise ValueError("control characters forbidden in " + errname)
+        if in_table_c3(c):
+            raise ValueError("private use characters forbidden in " + errname)
+        if in_table_c4(c):
+            raise ValueError("non-char code points forbidden in " + errname)
+        if in_table_c5(c):
+            raise ValueError("surrogate codes forbidden in " + errname)
+        if in_table_c6(c):
+            raise ValueError("non-plaintext chars forbidden in " + errname)
+        if in_table_c7(c):
+            # XXX: should these have been caught by normalize?
+            # if so, should change this to an assert
+            raise ValueError("non-canonical chars forbidden in " + errname)
+        if in_table_c8(c):
+            raise ValueError("display-modifying / deprecated chars "
+                             "forbidden in" + errname)
+        if in_table_c9(c):
+            raise ValueError("tagged characters forbidden in " + errname)
+
+        # do bidi constraint check chosen by bidi init, above
+        if is_forbidden_bidi_char(c):
+            raise ValueError("forbidden bidi character in " + errname)
+
+    return data
+
 #==========================================================
 #bytes helpers
 #==========================================================
@@ -543,7 +664,6 @@ BEMPTY = b('')
 
 #helpers for joining / extracting elements
 bjoin = BEMPTY.join
-ujoin = u''.join
 
 def belem_join(elems):
     """takes series of bytes elements, returns bytes.
