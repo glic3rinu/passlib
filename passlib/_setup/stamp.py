24c16e16f8e987fe7aeecced1bd7a1959bda82b1
Eli Collins <elic@assurancetechnologies.com>
2012-02-09 15:25:38 -0500

    tweak to setup script

--- passlib/_setup/stamp.py
+++ passlib/_setup/stamp.py
@@ -25,33 +25,33 @@ def stamp_source(base_dir, version, dry_run=False):
     path = os.path.join(base_dir, "passlib", "__init__.py")
     with open(path) as fh:
         input = fh.read()
-    output = re.sub('(?m)^__version__\s*=.*$',
+    output, count = re.subn('(?m)^__version__\s*=.*$',
                     '__version__ = ' + repr(version),
                     input)
-    assert output != input, "failed to match"
+    assert count == 1, "failed to replace version string"
     if not dry_run:
         os.unlink(path) # sdist likes to use hardlinks
         with open(path, "w") as fh:
             fh.write(output)
-   
+
 def stamp_distutils_output(opts, version):
-   
+
     # subclass buildpy to update version string in source
     _build_py = get_command_class(opts, "build_py")
     class build_py(_build_py):
         def build_packages(self):
             _build_py.build_packages(self)
-            stamp_source(self.build_lib, version, self.dry_run)            
+            stamp_source(self.build_lib, version, self.dry_run)
     opts['cmdclass']['build_py'] = build_py
-    
+
     # subclass sdist to do same thing
     _sdist = get_command_class(opts, "sdist")
-    class sdist(_sdist):        
+    class sdist(_sdist):
         def make_release_tree(self, base_dir, files):
             _sdist.make_release_tree(self, base_dir, files)
             stamp_source(base_dir, version, self.dry_run)
     opts['cmdclass']['sdist'] = sdist
-    
+
 #=========================================================
 # eof
 #=========================================================
